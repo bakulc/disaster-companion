f10ca999094741741d2aa8a4422909297f70e3cf
hitesh <hitesh.student.ece17@itbhu.ac.in>
2018-10-23 21:35:08 +0530

    Add messaging activities, chat head shape and layout

---
+++ app/src/main/java/com/example/bakul/disastercompanion/sendSignal.java
@@ -0,0 +1,13 @@
+package com.example.bakul.disastercompanion;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sendSignal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_signal);
+    }
+}
